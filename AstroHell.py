ba7dbfd7b71a5f79d8c0de842e3daa9cbf179881
Leonardo Meato <leomeato@gmail.com>
2022-10-07 20:56:37 -0300

    menu sem os sprites

--- AstroHell.py
+++ AstroHell.py
@@ -6,9 +6,13 @@ janela = Window(1280, 720)
 
 Mouse = janela.get_mouse()
 
+teclado = janela.get_keyboard()
+
 fundo = Sprite("mapa-fundo.png")
 
 while True:
     fundo.draw()
     janela.update()
     #resposta = menu(BotaoJogar, BotaoConfiguraçoes, BotaoSair, Mouse, janela, logo)
+    #if resposta == 1:
+    #    jogar(teclado, Mouse, janela)
