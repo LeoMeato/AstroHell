709e8c25e7badef86dd0a6321542f2f7defc870c
Leonardo Meato <leomeato@gmail.com>
2022-10-07 20:47:53 -0300

    menu sem os sprites

--- AstroHell.py
+++ AstroHell.py
@@ -4,9 +4,11 @@ from menu import *
 
 janela = Window(1280, 720)
 
+Mouse = janela.get_mouse()
+
 fundo = Sprite("mapa-fundo.png")
 
 while True:
     fundo.draw()
     janela.update()
-    #resposta = menu()
+    #resposta = menu(BotaoJogar, BotaoConfiguraçoes, BotaoSair, Mouse, janela, logo)
--- menu.py
+++ menu.py
@@ -1,14 +1,15 @@
-def menu(BotaoJogar, BotaoConfiguraçoes, BotaoSair, Mouse, janela):
+def menu(BotaoJogar, BotaoConfiguraçoes, BotaoSair, Mouse, janela, logo):
     resposta = 0
+    logo.draw()
     BotaoJogar.draw()
     BotaoConfiguraçoes.draw()
     BotaoSair.draw()
     if Mouse.is_button_pressed(1) and janela.width/2 - BotaoJogar.width/2 <= Mouse.get_position()[0] <= janela.width/2 + BotaoJogar.width/2:
-        if janela.height/2 - 300 <= Mouse.get_position()[1] <= janela.height/2 - 300 + BotaoJogar.height:
+        if janela.height/2 - 300 <= Mouse.get_position()[1] <= janela.height/2 - 100 + BotaoJogar.height:
             resposta = 1
-        elif janela.height/2 - 150 <= Mouse.get_position()[1] <= janela.height/2 - 150 + BotaoConfiguraçoes.height:
+        elif janela.height/2 - 150 <= Mouse.get_position()[1] <= janela.height/2 + 50 + BotaoConfiguraçoes.height:
             resposta = 2
-        elif janela.height/2 + 150 <= Mouse.get_position()[1] <= janela.height/2 + 150 + BotaoSair.height:
+        elif janela.height/2 + 150 <= Mouse.get_position()[1] <= janela.height/2 + 200 + BotaoSair.height:
             resposta = 3
     while Mouse.is_button_pressed(1):
         janela.update()
